c666cf8fb42aa3160a192741e07132d6acb342b9
Katherine Leyonmark <leyonmarkk@allegheny.edu>
2022-01-18 11:32:46 -0500

    formatting data

--- src/comp.r
+++ src/comp.r
@@ -4,8 +4,8 @@
 # Senior Comprehensive Project Code
 
 rm(list = ls()) # remove variables stored in memory.
-graphics.off()
-cat("\014")
+graphics.off() # clear out all plots from previous work.
+cat("\014") # clear the console
 
 # adding libraries here
 library(tidyverse)
@@ -17,3 +17,11 @@ library(psych)
 # Load CSV data
 full_data <- read.csv(file.choose())
 View(full_data)
+#filter out variables being used for project
+comp_data <- select(full_data, -(ID), -(Dt_Customer), -(Recency), -(NumWebVisitsMonth), -(AcceptedCmp3), -(AcceptedCmp4), -(AcceptedCmp5), -(AcceptedCmp1), -(AcceptedCmp2), -(Response), -(Complain))
+
+#filter out a subset of data with only numerical values
+numeric <- select(comp_data, -(Country), -(Education), -(Marital_Status))
+
+#full correlation set via cor plot
+corPlot(numeric)
